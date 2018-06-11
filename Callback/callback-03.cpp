695efc26621ed8814991a37c1ab8b4d88aec3e45
Karthick Kumaran <asvkarthick@gmail.com>
2018-06-10 21:15:00 -0700

    Added another code for callback with arguments

---
+++ Callback/callback-03.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <functional>
+
+using namespace std::placeholders;
+
+class One
+{
+public:
+    One()
+    {
+        callback = nullptr;
+    }
+
+    void RegisterCallback(std::function<int (int, int)> f)
+    {
+        callback = f;
+    }
+
+    int Call(int a, int b)
+    {
+        if(callback != nullptr) return callback(a, b);
+    }
+private:
+    std::function<int (int, int)> callback;
+};
+
+class Two
+{
+public:
+    int Callback(int a, int b)
+    {
+        std::cout << __func__ << std::endl;
+        return a + b;
+    }
+
+    Two()
+    {
+        auto callback = std::bind(&Two::Callback, this, _1, _2);
+        one.RegisterCallback(callback);
+    }
+
+    int Call(int x, int y)
+    {
+        one.Call(x, y);
+    }
+private:
+    One one;
+};
+
+int main()
+{
+    Two t;
+    std::cout << t.Call(10, 20) << std::endl;
+    return 0;
+}
