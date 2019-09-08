f0bb53426521ffde73fdf48a6a640d148cb3244f
Karthick Kumaran <asvkarthick@gmail.com>
2019-09-08 00:02:53 -0700

    Added another vector program to insert element
    Signed-off-by: Karthick Kumaran <asvkarthick@gmail.com>

---
+++ CPP_STL/vector/vector-03.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+int main(void)
+{
+    std::vector<int> v;
+
+    std::cout << "Size of vector = " << v.size() << std::endl;
+
+    v.assign(10, 2);
+
+    std::cout << "Size of vector = " << v.size() << std::endl;
+
+    for(std::vector<int>::iterator it = v.begin(); it != v.end(); it++)
+        std::cout << *it << " ";
+    std::cout << std::endl;
+
+    // Inserting element at the beginning
+    v.insert(v.begin(), 3);
+
+    for(std::vector<int>::iterator it = v.begin(); it != v.end(); it++)
+        std::cout << *it << " ";
+    std::cout << std::endl;
+
+    return 0;
+}
