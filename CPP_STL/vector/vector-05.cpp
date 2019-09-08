524475df2673dc606a7b5e0f80a88b7007ca1fae
Karthick Kumaran <asvkarthick@gmail.com>
2019-09-08 00:28:18 -0700

    Added another vector program to pop elements
    Signed-off-by: Karthick Kumaran <asvkarthick@gmail.com>

---
+++ CPP_STL/vector/vector-05.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+int main(void)
+{
+    std::vector<int> v;
+
+    std::cout << "Size of vector = " << v.size() << std::endl;
+
+    v.assign(10, 2);
+
+    std::cout << "Size of vector = " << v.size() << std::endl;
+
+    for(std::vector<int>::iterator it = v.begin(); it != v.end(); it++)
+        std::cout << *it << " ";
+    std::cout << std::endl;
+
+    // Inserting element at the beginning
+    v.insert(v.begin(), 3);
+    // Insert at the third position
+    v.insert(v.begin() + 2, 5);
+
+    // Pushing element to the end
+    v.push_back(100);
+    v.push_back(200);
+
+    for(std::vector<int>::iterator it = v.begin(); it != v.end(); it++)
+        std::cout << *it << " ";
+    std::cout << std::endl;
+
+    // Popping the last element
+    v.pop_back();
+
+    for(std::vector<int>::iterator it = v.begin(); it != v.end(); it++)
+        std::cout << *it << " ";
+    std::cout << std::endl;
+
+    return 0;
+}
