e1a567d4e6c30e97cd117996c9ecab80873bab27
Karthick Kumaran <asvkarthick@gmail.com>
2019-09-09 18:30:22 -0700

    Added vector program to demonstrate v.data()
    Signed-off-by: Karthick Kumaran <asvkarthick@gmail.com>

---
+++ CPP_STL/vector/vector-08.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+
+int main(void)
+{
+    std::vector<int> v;
+
+    std::cout << "Size of vector = " << v.size() << std::endl;
+
+    v.assign(10, 2);
+
+    std::cout << "Size of vector = " << v.size() << std::endl;
+
+    for(std::vector<int>::iterator it = v.begin(); it != v.end(); it++)
+        std::cout << *it << " ";
+    std::cout << std::endl;
+
+    // Inserting element at the beginning
+    v.insert(v.begin(), 3);
+    // Insert at the third position
+    v.insert(v.begin() + 2, 5);
+
+    // Pushing element to the end
+    v.push_back(100);
+    v.push_back(200);
+
+    for(std::vector<int>::iterator it = v.begin(); it != v.end(); it++)
+        std::cout << *it << " ";
+    std::cout << std::endl;
+
+    // Popping the last element
+    v.pop_back();
+
+    for(std::vector<int>::iterator it = v.begin(); it != v.end(); it++)
+        std::cout << *it << " ";
+    std::cout << std::endl;
+
+    // Erase the first element
+    v.erase(v.begin());
+    std::cout << "Erased first element" << std::endl;
+
+    // Print all the elements
+    for(std::vector<int>::iterator it = v.begin(); it != v.end(); it++)
+        std::cout << *it << " ";
+    std::cout << std::endl;
+
+    // Printing all the elements through C pointer type
+    std::cout << "Printing vector elements with C pointer" << std::endl;
+    int *ptr = v.data();
+    for(int i = 0; i < v.size(); i++)
+        std::cout << ptr[i] << " ";
+    std::cout << std::endl;
+
+    // Clear all the elements in the vector
+    v.clear();
+    std::cout << "Cleared all the elements in the vector" << std::endl;
+
+    // Print all the elements
+    for(std::vector<int>::iterator it = v.begin(); it != v.end(); it++)
+        std::cout << *it << " ";
+    std::cout << std::endl;
+
+    return 0;
+}
